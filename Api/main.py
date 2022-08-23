8326d4bef94a1cf098bd36a3bdf707775aefccd7
unknown <alexandre.roberto115@gmail.com>
2022-08-23 18:22:54 -0300

    rota padrão de get

--- Api/main.py
+++ Api/main.py
@@ -1,8 +1,18 @@
-from distutils.log import debug
 from flask import Flask
+from flask import jsonify,make_response,request
+from flask_cors import CORS
+from database import DbVisie
 
+db = DbVisie()
 app = Flask(__name__)
+CORS(app,resources={r'/*' : {"origins": '*'}})
 
+
+
+@app.route("/AllPeople")
+def get_all_people():
+    x = db.get_all_people()
+    return make_response(x,200)
 
 
 
